7b683542951979a789cb922fbb1d436b6a094aa0
yujinclare <yujinclare@gmail.com>
2025-03-14 21:22:56 +0900

    Update my_tts.py

--- pages/my_tts.py
+++ pages/my_tts.py
@@ -1,7 +1,14 @@
 
 import streamlit as st
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import qrcode
+from PIL import Image
+from wordcloud import WordCloud
+import streamlit.components.v1 as components  # For embedding YouTube videos
 from gtts import gTTS
+import io
 
 tabs = st.tabs(["📈 Welcome", "⏳ TTS"])
 with tabs[0]:
